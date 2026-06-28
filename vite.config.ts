e602958a996a7fa216763a6bf510accc5db5f9e1
Zara Ayyaz <zaraayyaz2@gmail.com>
2026-06-28 18:46:08 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {},
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
 })
